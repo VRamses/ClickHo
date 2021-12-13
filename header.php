e81645d2957969d3b306ab5c58216aec1d1d9009
Victor Ramsés <82965282+VRamses@users.noreply.github.com>
2021-12-12 21:21:21 -0300

    Upload dos arquivos
    Primeiro upload dos arquivos. A partir daqui, daremos commits para facilitar a organização.

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>ClickHo</title>
+
+    <style>
+        @font-face {
+            font-family:'Christmas';
+            src: url('fonts/happy.ttf') format('opentype');
+            font-weight: normal;
+            font-style: normal;
+        }
+        
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h1 {
+            font-family: 'Christmas', Times, serif;
+            font-size: 3em;
+            font-weight: normal;
+        }
+    </style>
+
+
+    <meta charset="UTF-8">
+    <meta name="author" content="KBSilva" >
+    <meta name="description" content="website de natal">
+    <meta name="keywords" content="natal, web, html, css">
+
+    <link rel="stylesheet" href="css/estilo.css">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Mountains+of+Christmas:wght@400;700&display=swap" rel="stylesheet">
+
+   <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Poppins:wght@100;300&display=swap" rel="stylesheet">
+
+</head>
